9596cc52f9066056febe1806ec6934ff3b86ca28
Steve Lancashire <stlancashire@gmail.com>
2022-12-14 16:09:43 +1100

    Working version of go_make_mod.sh, creates README, initialises go mod and git repo, has a bug with variable substitution in README

--- go_make_mod/go_make_mod.sh
+++ go_make_mod/go_make_mod.sh
@@ -9,24 +9,26 @@ if [ -z "$1" ]
 fi
 
 NEW_PROJECT=$1
-NEW_PROJECT_PATH="./$1"
-MAKE_FILE_PATH="~/src/bash_scripts/go_make_mod/"
+NEW_PROJECT_PATH=./$1
+MAKE_FILE_PATH=~/src/bash_scripts/go_make_mod/
 GITHUB_USER=stevelan
 
-if [[ -f $NEW_PROJECT_PATH ]]
+echo "Making go module ($NEW_PROJECT)"
+
+if [[ -e "$NEW_PROJECT_PATH" ]]
 then
     echo "$NEW_PROJECT_PATH already exists on your filesystem."
     exit 2
 fi
 
-echo "Making go module ($NEW_PROJECT)"
 mkdir $NEW_PROJECT_PATH 
 echo "Copying makefile from $MAKE_FILE_PATH"
-cp $MAKE_FILE_PATH/makefile $NEW_PROJECT_PATH
+cp ${MAKE_FILE_PATH}makefile $NEW_PROJECT_PATH/
 echo "Copying main_archetype.go from $MAKE_FILE_PATH"
-cp $MAKE_FILE_PATH/main_archetype.go $NEW_PROJECT_PATH/$NEW_PROJECT.go
+cp ${MAKE_FILE_PATH}main_go_archetype ${NEW_PROJECT_PATH}/${NEW_PROJECT}.go
+
 echo "Making README.md"
-cat <<'EOF' > $NEW_PROJECT/README.md
+cat <<'EOF' > ${NEW_PROJECT_PATH}/README.md
 #$NEW_PROJECT.go
 Simple go script with single source and mod file in root directory
 ##Getting started
@@ -35,12 +37,17 @@ Build the project with `make build` will produce a binary in the root of the pro
 Clean the project with `make clean` 
 ##TODO - Adopt and describe a project layout from : https://github.com/golang-standards/project-layout
 EOF
-echo "Initialising go module $NEW_PROJECT"
-go mod init $NEW_PROJECT_PATH
-cd $NEW_PROJECT_PATH
+
 echo "Changing directories to $NEW_PROJECT_PATH"
+cd $NEW_PROJECT_PATH
+
+echo "Initialising go module $NEW_PROJECT"
+go mod init $NEW_PROJECT
+
 echo "Initialising git repo"
-git init && git add 
+git init && git add .
 git commit -m "Initial commit for $NEW_PROJECT"
 git remote add origin git@github.com:$GITHUB_USER/$NEW_PROJECT
-echo "Go module made"
+
+pwd
+echo "Go module made ($NEW_PROJECT)"
